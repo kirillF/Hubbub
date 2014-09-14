4b66b762f2d5523cd48df8478219aca1d7ba0d0a
kirillf <kirill.filimonov@gmail.com>
2014-09-14 21:38:02 +0400

    user registration command

--- grails-app/controllers/com/grails/UserController.groovy
+++ grails-app/controllers/com/grails/UserController.groovy
@@ -25,4 +25,56 @@ class UserController {
             return [ user: user ]
         }
     }
+
+    def register2 = { UserRegistrationCommand urc ->
+        if(urc.hasErrors()) {
+            return [ user: urc ]
+        } else {
+            def user =  new User(urc.properties)
+            user.profile = new Profile(urc.properties)
+
+            if (user.save()) {
+                flash.message = "User successfully created"
+                redirect(uri: "/")
+            } else {
+                return [ user: urc ]
+            }
+        }
+    }
+}
+
+class UserRegistrationCommand {
+    String userId
+    String password
+    String passwordRepeat
+
+    byte[] photo
+    String fullName
+    String bio
+    String homepage
+    String email
+    String timezone
+    String country
+    String jabberAddress
+
+    static constraints = {
+        userId(size: 3..20)
+
+        password(size: 6..8, blank: false,
+            validator: { passwrd, urc ->
+                return passwd != urc.userId
+            })
+        passwordRepeat(nullable: false,
+            validator: { passwd, urc ->
+                return passwd == urc.password
+            })
+        fullName(nullable: true)
+        bio(nullable: true, maxSize: 1000)
+        homepage(url: true, nullable: true)
+        email(email: true, nullable: true)
+        photo(nullable: true)
+        country(nullable: true)
+        timezone(nullable: true)
+        jabberAddress(nullable: true)
+   }
 }
